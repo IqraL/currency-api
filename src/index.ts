c75794c17c215bf6cdc319b9777268bcbb63978f
IqraL <IqraLatif159@gmail.com>
2022-05-16 10:09:01 +0100

    resolved heroku port issue

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,19 @@ import { Client } from "pg";
 import WebSocket, { WebSocketServer } from "ws";
 import { Server } from "socket.io";
 
-const clientServer = new Server(4000, {
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+const port = process.env.PORT || 5000;
+
+const server = require("http").createServer(app);
+
+const clientServer = new Server(server, {
   cors: { origin: "*" },
 });
+
+server.listen(port);
 
 clientServer.on("connection", (socket) => {
   const coincapWS = new WebSocket("wss://ws.coincap.io/prices?assets=ALL");
@@ -21,12 +31,6 @@ clientServer.on("connection", (socket) => {
   });
 });
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded());
-
-const port = 5000;
-
 const client = new Client({
   host: "tai.db.elephantsql.com",
   port: 5432,
@@ -36,8 +40,6 @@ const client = new Client({
 });
 
 client.connect();
-
-app.listen(port, () => console.log(`Running on port ${port}`));
 
 app.get("/", async (req, res) => {
   res.send();
